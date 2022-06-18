6bf3655eec33b101ba39326ce1dc4823018542fb
Zakrok09 <zakrokliga@gmail.com>
2022-06-18 12:03:07 +0300

    Add netlify build and adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
